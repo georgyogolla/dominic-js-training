e2c2bcfdce7ee6e1199122f6ff121d39f066110d
georgyogolla <georgyogolla@gmail.com>
2024-08-09 18:37:23 +0300

    Array static methods, Array.from()

--- arrays.js
+++ arrays.js
@@ -43,9 +43,17 @@ colors.length = 5;
 colors[4] = 'pink';
 
 // .forEach() won't visit any empty slots in the array
-colors.forEach((color, index) => {
-    console.log(`Index: ${index}, Color: ${color}`);
-});
+// colors.forEach((color, index) => {
+//     console.log(this);
+//     console.log(`Index: ${index}, Color: ${color}`);
+
+// });
+
+// colors.forEach(function(color, index){
+//     console.log(this);
+//     console.log(`Index: ${index}, Color: ${color}`);
+    
+// });
 
 // Newer methods (e.g. keys) do not treat empty slots specially and treat them as if they contain undefined. 
 // Methods that conflate empty slots with undefined elements include the following: entries(), fill(), find(), findIndex(), findLast(), findLastIndex(), includes(), join(), keys(), toLocaleString(), toReversed(), toSorted(), toSpliced(), values(), and with().
@@ -70,7 +78,44 @@ nums.push(5);
 console.log(nums);// [ 1, 2, 3, 5 ]
 console.log(nums2); // [ 1, 2, 3, 5 ]
 
+// STATIC METHODS
+// Static methods can only be called with the prefix Array. 
+// They relate directly to an Array as a whole, rather than a specific array that you have created or are working with.
 
+// The Array.from() 
+// - static method creates a new, shallow-copied Array instance from an iterable or array-like object
+// SYNTAX
+// Array.from(arrayLike)
+// Array.from(arrayLike, mapFn)
+// Array.from(arrayLike, mapFn, thisArg)
 
+// To convert an ordinary object that's not iterable or array-like to an array (by enumerating its property keys, values, or both), use Object.keys(), Object.values(), or Object.entries(). 
+// To convert an async iterable to an array, use Array.fromAsync().
 
+const obj1 = {a:1, b:2, c:3};
+// use Object.keys to convert the obj1 keys to an iterable array
+console.log(Object.keys(obj1));//keys as an array
 
+// use Object.values to convert the obj1 values to an iterable array
+console.log(Object.values(obj1));//values as an array
+
+// use Object.entries to convert the obj1 entries to an iterable array
+console.log(Object.entries(obj1));//entries as an array
+
+// convert a string into an array
+console.log(Array.from("Hello, World!"));// ["H", "e", "l", "l", "o", ",", " ", "W", "o", "r", "l", "d", "!"]
+
+// use Array.from() with Set()
+const set = new Set(["hello", "hello", "kenya"]);
+console.log(Array.from(set));// [ 1, 2, 3, 4, 5 ]
+
+console.log(Array.from("kenya, yetu"));
+
+// using Array.from() with map()
+const map = new Map([[1, "a"], [3, "b"], [5, "c"]]);// Map expets array like key-value pairs
+console.log(Array.from(map));
+
+// get keys from map variable
+console.log(Array.from(map.keys()));
+// get values from map variable
+console.log(Array.from(map.values()));
